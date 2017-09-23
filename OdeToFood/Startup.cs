9bc245e6b9a9810bd93bbe0ec57540053d957a8c
Salman C <salman.cheema@gmail.com>
2017-09-23 21:11:03 +0100

    Add project files.

---
+++ OdeToFood/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using OdeToFood.Services;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("mysettings.json")
+                .AddEnvironmentVariables();
+
+            /*
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("mysettings.json")
+                .SetBasePath(Directory.GetCurrentDirectory()); 
+            */
+
+            Configuration = builder.Build();
+        }
+
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddSingleton(provider => Configuration);
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeter greeter)
+        {
+            loggerFactory.AddConsole();
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWelcomePage();
+            
+            //app.UseIISPlatformHandler();
+            app.Run(async (context) =>
+            {
+                //var greeting = Configuration["greeting"];
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+    }
+}
